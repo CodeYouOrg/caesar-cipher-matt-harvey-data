6a41d233e36f35befc7d9b49c716a808c1d770d6
Matt Harvey <jmharv01@louisville.edu>
2024-08-02 13:38:51 -0700

    5

--- cipher.py
+++ cipher.py
@@ -1,1 +1,17 @@
-# add your code here
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+shift = input("Please enter the number of places to shift value: ")
+
+coded = 'zebras are neat!'
+
+sentence = ''
+
+for letter in coded.lower():
+    n = int(shift) % 26
+    if letter in alphabet:
+        m = alphabet.index(letter)
+        sentence += alphabet[(m + n) % 26]
+    else:
+        sentence += letter
+
+print(sentence)# add your code here
